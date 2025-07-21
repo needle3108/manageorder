58a5e3cf4e418630be1f2827b37697b1af458555
needle3108 <92887393+needle3108@users.noreply.github.com>
2025-07-21 14:39:48 +0200

    Init v2

--- customerorder/src/main/java/com/project/customerorder/CustomerorderApplication.java
+++ customerorder/src/main/java/com/project/customerorder/CustomerorderApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CustomerorderApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CustomerorderApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerorderApplication.class, args);
+    }
 
 }
