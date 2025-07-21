8860862b5450430522711030c06f7792339a1b09
needle3108 <92887393+needle3108@users.noreply.github.com>
2025-07-21 14:35:49 +0200

    Init commit

---
+++ manufacturingorder/src/main/java/com/project/manufacturingorder/ManufacturingorderApplication.java
@@ -0,0 +1,13 @@
+package com.project.manufacturingorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManufacturingorderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManufacturingorderApplication.class, args);
+    }
+
+}
---
+++ manufacturingorder/src/test/java/com/project/manufacturingorder/ManufacturingorderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.manufacturingorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManufacturingorderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
