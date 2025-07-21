f968087af02e0af4a1bf8645fe7dd83232266e0d
needle3108 <92887393+needle3108@users.noreply.github.com>
2025-07-21 20:02:14 +0200

    create postgreSQL database

---
+++ init-db/init.sql
@@ -0,0 +1,22 @@
+CREATE SCHEMA IF NOT EXISTS customerorder;
+
+CREATE TABLE IF NOT EXISTS customer_order (
+    id UUID PRIMARY KEY,
+    status VARCHAR(50) NOT NULL
+    );
+
+CREATE TABLE IF NOT EXISTS order_line (
+    id SERIAL PRIMARY KEY,
+    order_id UUID REFERENCES customer_order(id) ON DELETE CASCADE,
+    product_id UUID NOT NULL,
+    quantity INTEGER NOT NULL
+    );
+
+INSERT INTO customer_order (id, status) VALUES
+('11111111-1111-1111-1111-111111111111', 'CREATED'),
+('22222222-2222-2222-2222-222222222222', 'CONFIRMED');
+
+INSERT INTO order_line (order_id, product_id, quantity) VALUES
+('11111111-1111-1111-1111-111111111111', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 10),
+('11111111-1111-1111-1111-111111111111', 'bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb', 5),
+('22222222-2222-2222-2222-222222222222', 'cccccccc-cccc-cccc-cccc-cccccccccccc', 2);
