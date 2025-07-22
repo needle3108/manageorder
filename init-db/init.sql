a91a6169dce252e0797a437bc7060cbc9517054e
needle3108 <92887393+needle3108@users.noreply.github.com>
2025-07-22 11:23:49 +0200

    define DB for manufacturingorder

--- init-db/init.sql
+++ init-db/init.sql
@@ -20,3 +20,10 @@ INSERT INTO order_line (order_id, product_id, quantity) VALUES
 ('11111111-1111-1111-1111-111111111111', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 10),
 ('11111111-1111-1111-1111-111111111111', 'bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb', 5),
 ('22222222-2222-2222-2222-222222222222', 'cccccccc-cccc-cccc-cccc-cccccccccccc', 2);
+
+CREATE SCHEMA IF NOT EXISTS manufacturingorder;
+
+CREATE TABLE IF NOT EXISTS manufacturing_order (
+    id UUID PRIMARY KEY,
+    status VARCHAR(50) NOT NULL
+    );
